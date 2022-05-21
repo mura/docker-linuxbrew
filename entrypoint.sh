4191ffcdffeaee56d53afbc2ed1aa4be41643d84
Yohei Murayama <muracchi@gmail.com>
2022-05-21 21:43:45 +0900

    feat: ユーザー作成をメソッド化

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-if [[ -n "$LOGIN" && ! $(id $LOGIN >/dev/null 2>&1) ]]; then
+
+create_user () {
+  if [[ -z "$LOGIN" ]]; then
+    return
+  fi
+  if $(id $LOGIN >/dev/null 2>&1); then
+    return
+  fi
+
   [ -f /app/initialize.sh ] && source /app/initialize.sh
   if [[ -z "$HOMEDIR" ]]; then
     export HOMEDIR="/home/$LOGIN"
   fi
   useradd -u "$UID" -g "$GROUP" -G "$GROUPS" -d "$HOMEDIR" -s "$LOGIN_SHELL" "$LOGIN"
   echo "$LOGIN:$PASSWORD" | chpasswd
-fi
+}
+
+create_user
 
 if [ ! -f "/etc/ssh/sshd_config" ];then
   cp -rp /app/ssh/* /etc/ssh/
